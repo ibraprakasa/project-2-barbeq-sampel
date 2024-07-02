5639b3e694b6af65cb627778ebe149675b3c39d1
Ibra Prakasa <113502572+2101082006@users.noreply.github.com>
2024-07-02 22:48:31 +0700

    tes

---
+++ database/migrations/2024_02_29_033439_create_produk_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produks', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode')->unique();
+            $table->string('nama_produk');
+            $table->integer('harga');
+            $table->integer('stock');
+            $table->string('gambar')->default('no-images.jpg');
+            $table->string('detail');
+            $table->foreignId('kategori_id');
+            $table->foreignId('user_id')->default('id');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produks');
+    }
+};
